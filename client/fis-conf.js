ba26c9a4875dcea4973b4296f4ecbc0240b17d9c
kebin <kekobin@163.com>
2017-06-24 10:58:06 +0800

    update.

--- client/fis-conf.js
+++ client/fis-conf.js
@@ -1,5 +1,24 @@
-fis.set('projectName', '/{{-project_name-}}');//设置项目名称，也即静态资源发布的目录
+var projectName = '{{-project_name-}}';//设置项目名称，也即静态资源发布的目录
 
 // chrome下可以安装插件实现livereload功能
 // https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei
 fis.config.set('livereload.port', 35729);
+
+// 生产环境
+fis.media('prod')
+.match('!{*.{html,tpl}, js/mod/**}', {
+    domain: 'http://hd.huya.com/${projectname}',
+    deploy: fis.plugin('local-deliver', {
+        to: '../pub/'+projectName+'/'
+    })
+})
+.match('widget/**.tpl', {
+    deploy: fis.plugin('local-deliver', {
+        to: '../pub/'+projectName+'View/'
+    })
+}).match('/(**.html)', {
+    release: '/$1',
+    deploy: fis.plugin('local-deliver', {
+        to: '../pub/'+projectName+'View/'
+    })
+});
